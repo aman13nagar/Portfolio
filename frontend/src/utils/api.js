9b9d2ad64c948a8c2f8fa34ff26ad677e42b99a7
Aman Nagar <122695931+aman13nagar@users.noreply.github.com>
2024-07-03 17:21:42 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,17 +1,5 @@
 import axios from 'axios';
-
-// Set up an instance of Axios with the base URL of your backend
 const api = axios.create({
   baseURL: 'https://portfolio-psi-roan-71.vercel.app/api', 
 });
-
-// Add a request interceptor to include the token in headers
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers['x-auth-token'] = token;
-//   }
-//   return config;
-// });
-
 export default api;
